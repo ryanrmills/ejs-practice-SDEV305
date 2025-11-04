5b2c9fd7c45e85e97e8f955bde0badc57e034111
ryanrmills <ryanroymills@gmail.com>
2025-11-04 11:36:05 -0800

    added friends

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get('/', (req, res)=> {
     res.render('home', { person });
 });
 
+app.get('/friends', (req, res) => {
+    const friends = ['Mickey', 'Goofy', 'Minnie', 'Daisy'];
+    res.render('friends', {friends});
+})
+
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`);
 });
